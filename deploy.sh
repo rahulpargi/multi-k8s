858575bc92ca98cd8a10b8175474b2262dae6b85
rahulpargi <rahulpargi@gmail.com>
2022-07-10 23:28:38 -0400

    deploy script fix

--- deploy.sh
+++ deploy.sh
@@ -1,7 +1,7 @@
 # Build Images
-docker build -t rahulpargi/client:latest rahulpargi/client:$SHA -f ./client/Dockerfile ./client
-docker build -t rahulpargi/server:latest rahulpargi/server:$SHA -f ./server/Dockerfile ./server
-docker build -t rahulpargi/worker:latest rahulpargi/worker:$SHA -f ./worker/Dockerfile ./worker
+docker build -t rahulpargi/client:latest -t rahulpargi/client:$SHA -f ./client/Dockerfile ./client
+docker build -t rahulpargi/server:latest -t rahulpargi/server:$SHA -f ./server/Dockerfile ./server
+docker build -t rahulpargi/worker:latest -t rahulpargi/worker:$SHA -f ./worker/Dockerfile ./worker
 
 # Push images to dockerhub
 docker push rahulpargi/client:latest
@@ -15,5 +15,5 @@ docker push rahulpargi/worker:$SHA
 # Apply config
 kubectl apply -f k8s
 kubectl set image deployments/server-deployment server=rahulpargi/server:$SHA
-kubectl set image deployments/client-deployment server=rahulpargi/client:$SHA
-kubectl set image deployments/worker-deployment server=rahulpargi/worker:$SHA
+kubectl set image deployments/client-deployment client=rahulpargi/client:$SHA
+kubectl set image deployments/worker-deployment worker=rahulpargi/worker:$SHA
